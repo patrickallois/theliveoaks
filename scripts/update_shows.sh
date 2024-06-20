5fafbf7061ef76012146c31cd6150cdb862371e0
Patrick Allois <patrickallois@gmail.com>
2024-06-19 20:58:15 -0400

    chore: add script notes

---
+++ scripts/update_shows.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# https://docs.aws.amazon.com/cli/latest/userguide/cli-services-s3-commands.html#using-s3-commands-managing-objects-copy
+# aws s3 cp <source> <target> [--options]
+
+# https://docs.aws.amazon.com/cli/latest/reference/cloudfront/list-distributions.html
+# aws cloudfront list-distributions
+
+# https://docs.aws.amazon.com/cli/latest/reference/cloudfront/create-invalidation.html
+# aws cloudfront create-invalidation \
+#     --distribution-id EDFDVBD6EXAMPLE \
+#     --paths "/example-path/example-file.jpg" "/example-path/example-file2.png"
